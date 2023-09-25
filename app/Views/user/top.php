57266fe1b518c974e3ea47f5c5114b2e3993bf0f
JohnFilhmar <144536090+JohnFilhmar@users.noreply.github.com>
2023-09-26 07:30:03 +0800

    progress on views

---
+++ app/Views/user/top.php
@@ -0,0 +1,36 @@
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Eflyer</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" type="text/css" href="<?php base_url() ?>/products/css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" type="text/css" href="<?php base_url() ?>/products/css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="<?php base_url() ?>/products/css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="<?php base_url() ?>/products/images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="<?php base_url() ?>/products/css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="<?php base_url() ?>/products/https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <!-- fonts -->
+      <link href="<?php base_url() ?>/products/https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+      <!-- font awesome -->
+      <link rel="stylesheet" type="text/css" href="<?php base_url() ?>/products/https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <!--  -->
+      <!-- owl stylesheets -->
+      <link href="<?php base_url() ?>/products/https://fonts.googleapis.com/css?family=Great+Vibes|Poppins:400,700&display=swap&subset=latin-ext" rel="stylesheet">
+      <link rel="stylesheet" href="<?php base_url() ?>/products/css/owl.carousel.min.css">
+      <link rel="stylesoeet" href="<?php base_url() ?>/products/css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="<?php base_url() ?>/products/https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+   </head>
---
+++ app/Views/userinterface.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<?= view('user/top'); ?>
+<body>
+    <?= view('user/banner'); ?>
+    <?= view('user/fashion'); ?>
+    <?= view('user/electronics'); ?>
+    <?= view('user/jewelry'); ?>
+    <?= view('user/footer'); ?>
+</body>
+</html>
