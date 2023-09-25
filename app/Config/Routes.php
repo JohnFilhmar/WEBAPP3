a7eb59e5a5561ef92f581150ad9ae3fab6807f57
ampelme <olajohnfilhmar@gmail.com>
2023-09-25 22:01:14 +0800

    initial commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,9 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/index', 'Home::index');
+$routes->get('/home', 'UserController::index');
